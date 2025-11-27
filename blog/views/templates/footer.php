71a342568cfccb34874c3ac3dd5096bc5af64a75
Samuel Ming <samuelming09@gmail.com>
2025-11-27 11:49:59 +0100

    Initial Commit

--- blog/views/templates/footer.php
+++ blog/views/templates/footer.php
@@ -1,8 +1,3 @@
 <div class="footer">
     <span class="copyright">©2025 by Samuel Ming</span>
-    <a class="github-link" href="https://github.com/samuelm203" target="_blank">
-        <img class="footer-img-github" width="20px"
-             src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
-             alt="GitHub Logo">
-    </a>
 </div>
